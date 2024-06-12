a0e265383bf3944083f1784f238a0d31add9e8ce
Dan Shedd <dan@dshedd.com>
2024-06-11 22:11:06 -0400

    Add back to title menu item

--- source/main.lua
+++ source/main.lua
@@ -5,4 +5,11 @@ Noble.Settings.setup({
 	text_speed = TextSpeed.Normal
 })
 
+local menu = playdate.getSystemMenu()
+
+local menuItem, error = menu:addMenuItem( "back to title", function()
+    GameController.saveData()
+    Noble.transition( TitleScene )
+end)
+
 Noble.new( SplashScene, 1.5, Noble.TransitionType.CROSS_DISSOLVE )
